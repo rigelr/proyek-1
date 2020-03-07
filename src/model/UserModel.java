3114818569691b45a4af891307008efec13aad98
addin309@gmail.com <addin309@gmail.com>
2020-03-07 18:53:14 +0700

    add user model, koneksi database, fix menu owner

--- src/model/UserModel.java
+++ src/model/UserModel.java
@@ -1,5 +1,58 @@
 package model;
 
 public class UserModel {
+    String iduser,username,password,nama;
+    boolean deleted_status;
+    int level;
+
+    public String getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(String iduser) {
+        this.iduser = iduser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public boolean isDeleted_status() {
+        return deleted_status;
+    }
+
+    public void setDeleted_status(boolean deleted_status) {
+        this.deleted_status = deleted_status;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+    
+    
 
 }
