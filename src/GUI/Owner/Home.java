62120ee05d61efa3753c62897d8e0e11029ea091
addinnt <addin309@gmail.com>
2020-04-13 19:33:23 +0700

    fix db connection

--- src/GUI/Owner/Home.java
+++ src/GUI/Owner/Home.java
@@ -42,6 +42,11 @@ public class Home extends javax.swing.JFrame {
         jButton1.setText("Transaksi");
 
         jButton3.setText("Menu");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("User");
 
@@ -98,6 +103,12 @@ public class Home extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        new EditMenu().show();
+        this.dispose();
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
