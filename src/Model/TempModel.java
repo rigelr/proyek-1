a3257e9b5559a988a1bc5cd510883e7fc1261d8f
amelrnt <ochaarn@gmail.com>
2020-05-13 21:32:25 +0700

    add new Model

---
+++ src/Model/TempModel.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author USER
+ */
+public class TempModel {
+    private String userid;
+    private String username;
+    private int hargaBayar;
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getHargaBayar() {
+        return hargaBayar;
+    }
+
+    public void setHargaBayar(int hargaBayar) {
+        this.hargaBayar = hargaBayar;
+    }
+    
+}
